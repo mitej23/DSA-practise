7b4b32a66ebbe8bf0237dfa7ddaf3b65572a91a7
mitej23 <mitejmadan@gmail.com>
2021-07-03 16:29:04 +0530

    stack using array

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -13,11 +13,53 @@ class stack{
             top = -1;
         }
 
-        
+        void push(int val){
+            if(top == n - 1){
+                cout<<"stack overflow"<<endl;
+                return;
+            }
+
+            top++;
+            arr[top] = val;
+        }
+        void pop(){
+            if(top == -1){
+                cout<<"stack overflow"<<endl;
+                return;
+            }
+            top--;
+        }
+        int Top(){
+            if(top == -1){
+                cout<<"stack is empty"<<endl;
+                return 0;
+            }
+            return arr[top];
+        }
+        bool empty(){
+            return top == -1;
+        }
+
+
 };
 
 int main(){
+    stack st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
 
+    cout<<st.Top()<<endl;
+
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+
+
+
+    cout<<st.empty()<<endl;
 
     return 0;
 }
