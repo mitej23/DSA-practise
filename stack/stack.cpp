f6165aa24c36ca396cb08209b4460b6b746372e5
mitej23 <mitejmadan@gmail.com>
2021-07-03 16:11:45 +0530

    added stack intro

---
+++ stack/stack.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+#define n 100
+
+class stack{
+    int* arr;
+    int top;
+
+    public:
+        stack(){
+            arr = new int[n];
+            top = -1;
+        }
+
+        
+};
+
+int main(){
+
+
+    return 0;
+}
