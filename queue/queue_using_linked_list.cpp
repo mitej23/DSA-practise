3e01a18d584b0acda4e6b5bee95ad2ae7da5e25c
mitej23 <mitejmadan@gmail.com>
2021-07-05 15:59:48 +0530

    added queue implementation using linked list

---
+++ queue/queue_using_linked_list.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node* next;
+        node(int num){
+            data = num;
+            next = NULL;
+        }
+};
+
+class queue{
+    node* front;
+    node* back;
+    public:
+        queue(){
+            front = NULL;
+            back = NULL;
+        }
+        void push(int insert){
+            node* n = new node(insert);
+            if(front == NULL){
+                back = n;
+                front = n;
+                return;
+            }
+            back->next = n;
+            back = n;
+        }
+        void pop(){
+            if(front == NULL){
+                cout<<"Queue underflow"<<endl;
+                return;
+            }
+            node* remove = front;
+            front = front->next;
+    
+            delete remove;
+        }
+    
+        int peek(){
+            if(front == NULL){
+                cout<<"No element in queue"<<endl;
+                return -1;
+            }
+            return front->data;
+        }
+        bool empty(){
+            if(front == NULL){
+                return true;
+            }
+            return false;
+        }
+};
+
+int main(){
+    queue q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+
+    cout<<q.peek()<<endl;
+    q.pop();
+
+    cout<<q.peek()<<endl;
+    q.pop();
+
+    cout<<q.peek()<<endl;
+    q.pop();
+    
+    cout<<q.peek()<<endl;
+    q.pop();
+
+
+    return 0;
+}
