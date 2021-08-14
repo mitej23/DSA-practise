f66863345ebec751900260e0bcd6f12b1f8102f2
mitej23 <mitejmadan@gmail.com>
2021-08-14 14:32:34 +0530

    added sol for graph using adjaceny matrix

---
+++ graphs/graphs_adj_matrix.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;
+    // n => Nodes
+    // m => Edges
+    cin >> n >> m;
+
+    int adj[n+1][n+1];
+
+    for(int i = 0;i < m;i++){
+        int u,v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+    return;
+}
