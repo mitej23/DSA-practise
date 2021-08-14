25bb239ad8f35edac3ebbabadf23858bcb2a973c
mitej23 <mitejmadan@gmail.com>
2021-08-15 01:01:09 +0530

    added dfs algorithm

---
+++ graphs/dfs.cpp
@@ -0,0 +1,27 @@
+// It is a revursive function
+
+class Solution{
+    void dfs(int node, vector<int> &vis, vector<int> adj[], vector<int> &storeDfs){
+        storeDfs.push_back(node);
+        vis[node] = 1;
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                dfs(it, vis, adj, storeDfs);
+            }
+        }
+    }
+
+    public:
+        vector<int> dfsOfGraph(int V,<int> adj[]){
+            // V -> No. of nodes
+            // adj -> adjacency list
+            vector<int> storeDfs;
+            vector<int> vis(V+1,0);
+            for(int i = 1;i <= V;i++){
+                if(!vis[i]){
+                    dfs(i , vis , adj, storeDfs);
+                }
+            }
+            return storeDfs;
+        }
+};
